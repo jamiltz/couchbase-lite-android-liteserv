4d09eab0f353a9f01916a29f07263afd791aed68
James Nocentini <james.nocentini@gmail.com>
2015-10-28 18:48:05 +0000

    attempt to add filter compiler

--- couchbase-lite-android-liteserv/src/main/java/com/couchbase/liteservandroid/MainActivity.java
+++ couchbase-lite-android-liteserv/src/main/java/com/couchbase/liteservandroid/MainActivity.java
@@ -9,19 +9,20 @@ import android.widget.TextView;
 import com.couchbase.lite.CouchbaseLiteException;
 import com.couchbase.lite.Database;
 import com.couchbase.lite.Manager;
+import com.couchbase.lite.ReplicationFilter;
+import com.couchbase.lite.ReplicationFilterCompiler;
 import com.couchbase.lite.View;
 import com.couchbase.lite.android.AndroidContext;
 import com.couchbase.lite.javascript.JavaScriptViewCompiler;
 import com.couchbase.lite.listener.Credentials;
 import com.couchbase.lite.listener.LiteListener;
-import com.couchbase.lite.listener.LiteServlet;
 
 import java.io.IOException;
 
 public class MainActivity extends Activity {
 
     private static final int DEFAULT_LISTEN_PORT = 5984;
-    private static final String DATABASE_NAME = "cblite-test";
+    private static final String DATABASE_NAME = "skybook";
     private static final String LISTEN_PORT_PARAM_NAME = "listen_port";
 
     private static final String LISTEN_LOGIN_PARAM_NAME = "username";
@@ -37,6 +38,12 @@ public class MainActivity extends Activity {
 
         // Register the JavaScript view compiler
         View.setCompiler(new JavaScriptViewCompiler());
+        Database.setFilterCompiler(new ReplicationFilterCompiler() {
+            @Override
+            public ReplicationFilter compileFilterFunction(String source, String language) {
+                return null;
+            }
+        });
 
         try {
             int port = startCBLListener(getListenPort());
@@ -74,11 +81,13 @@ public class MainActivity extends Activity {
         startDatabase(manager, DATABASE_NAME);
 
 
-        if (getLogin()!=null && getPassword()!=null){
-            allowedCredentials = new Credentials(getLogin(), getPassword());
-        } else{
-            allowedCredentials = new Credentials();
-        }
+//        if (getLogin()!=null && getPassword()!=null){
+//            allowedCredentials = new Credentials(getLogin(), getPassword());
+//        } else{
+//            allowedCredentials = new Credentials();
+//        }
+
+        allowedCredentials = new Credentials(null, null);
 
         LiteListener listener = new LiteListener(manager, suggestedListenPort, allowedCredentials);
 
